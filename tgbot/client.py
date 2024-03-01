f0033df82dc906efe02554f89ae2d04e9bf600aa
oonixxxxx <offconix@gmail.com>
2024-03-02 00:04:40 +0300

    debag client.py

--- tgbot/client.py
+++ tgbot/client.py
@@ -11,22 +11,14 @@ from aiogram.types import Message
 from aiogram.utils.markdown import hbold
 from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
-from client.config import TOKEN
+from tgbot.config import TOKEN
 
 # All handlers should be attached to the Router (or Dispatcher)
 dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    """
-    This handler receives messages with `/start` command
-    """
-    # Most event objects have aliases for API methods that can be called in events' context
-    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
-    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
-    # method automatically or call API method directly via
-    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
-    await message.answer(f"Hello, {hbold(message.from_user.full_name)}!. \n typing /menu")
+    await message.answer(f"Hello, {hbold(message.from_user.full_name)}! Я бот длля продажи предметов в локальном магазине. \n что бы перейти к выполнению заказа напиши 'В меню'")
 
 
 @dp.message(F.text == 'В меню')
