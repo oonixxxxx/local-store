014376faf20cfdcc2e2aa2c54907cabfade009ba
oonixxxxx <offconix@gmail.com>
2024-03-02 16:55:14 +0300

    add all function on menu buttons

--- app/handlers.py
+++ app/handlers.py
@@ -6,16 +6,10 @@ from aiogram.types import ReplyKeyboardRemove
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 
 
-main = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text='Каталог')],
-    [KeyboardButton(text='Корзина'), KeyboardButton(text='Контакт')]
-]
+main = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Каталог')],
+                                     [KeyboardButton(text='Корзина'), KeyboardButton(text='Контакт')]],
         resize_keyboard=True,
         input_field_placeholder='Выберите пунт меню.')
-
-settings = ReplyKeyboardMarkup(inline_keyboard=[
-    InlineKeyboardButton(text='Ссылка на видео', url='https://www.youtube.com/watch?v=qRW20XUb-lQ')
-])
 
 
 router = Router()
@@ -25,6 +19,18 @@ async def command_start_handler(message: Message) -> None:
     await message.answer(f"Hello, {hbold(message.from_user.full_name)}! Я бот длля продажи предметов в локальном магазине. \n что бы перейти к выполнению заказа напиши 'В меню'", reply_markup=main)
 
 
+@router.message(F.text == 'Каталог')
+async def get_catalog(message: Message):
+    await message.answer('Функция каталога')
+
+@router.message(F.text == 'Корзина')
+async def get_busket(message: Message):
+    await message.answer('Функция Корзины')
+
+@router.message(F.text == 'Контакт')
+async def get_contact(message: Message):
+    await message.answer('Функция контакты')
+
 @router.message(F.text == 'В меню')
 async def get_menu(message: Message):
-    await message.answer('you in menu',reply_markup=settings)
+    await message.answer('you in menu')
