e4b36ddead90028500c62dafdf0df67132baacf8
oonixxxxx <offconix@gmail.com>
2024-04-07 11:59:57 +0300

    refactor code

--- app/articuls.py
+++ app/articuls.py
@@ -1,3 +1,19 @@
 dictionary_of_articuls = {
     'articul': 'name of shoes'
 }
+
+class Articules:
+    articules_dict_name = {
+        '563564645645': 'shoes 1',
+        '5646764746746': 'shoes 2',
+        '564465556666': 'shoes 3',
+        '564465559999': 'shoes 4'
+        }
+
+
+    articules_dict_prize = {
+        'shoes 1': '77$',
+        'shoes 2': '86$',
+        'shoes 3': '76$',
+        'shoes 4': '97$'
+    }
